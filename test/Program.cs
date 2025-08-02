b17be981c574ac25a02c165f1902dde0b35e52c7
Anthony Limanto <s3898395@student.rmit.edu.au>
2025-08-03 00:22:28 +1000

    created test cs project to run StringFormatter.cs

---
+++ test/Program.cs
@@ -0,0 +1,6 @@
+﻿using PointsBet_Backend_Online_Code_Test;
+
+string[] items = { "item 1", "item 2", "item 3", "another item", "item 5" };
+string result = StringFormatter.ToCommaSeparatedList(items, "\"");
+
+Console.WriteLine(result);
---
+++ test/StringFormatter.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+
+namespace PointsBet_Backend_Online_Code_Test
+{
+
+    /*
+    Improve a block of code as you see fit in C#.
+    You may make any improvements you see fit, for example:
+      - Cleaning up code
+      - Removing redundancy
+      - Refactoring / simplifying
+      - Fixing typos
+      - Any other light-weight optimisation
+    */
+    public class StringFormatter
+    {
+
+        // Code to improve
+        // Fix typo in Separated
+        // Changing parameter name quote to wrapper since we may want to use this function with string than just single or double quotes
+        public static string ToCommaSeparatedList(string[] items, string wrapper)
+        {
+            // Added null check for safety
+            if (items == null || items.Length == 0)
+            {
+                return string.Empty;
+            }
+
+            // Reduced to a one liner to simplify the code, used named variables instead of 0's and 1's to improve readablilty
+            return string.Join(", ", items.Select(item => $"{wrapper}{item}{wrapper}"));
+
+            // Old code was verbose 
+            // StringBuilder qry = new StringBuilder("{0}{1}{0}", quote, items[0]);
+
+            // if (items.Length > 1)
+            // {
+            //     for (int i = 1; i < items.Length; i++)
+            //     {
+            //         qry.Append(string.Format(", {0}{1}{0}", quote, items[i]));
+            //     }
+            // }
+
+            // Since we're not using StringBuilder anymore qry.ToString() is redundant
+            // return qry.ToString();
+        }
+    }
+
+}
